2e5f9cc423bf65a001b9d14f1e0c9a90ec783f29
Piotr Szostak <pio.szo@proton.me>
2023-11-24 19:47:33 +0100

    Create operations funcs

--- kalk.py
+++ kalk.py
@@ -1,21 +1,32 @@
-import logging
+import logging 
+logging.basicConfig(level=logging.INFO) 
 
-logging.basicConfig(level=logging.INFO)
+def add(a, b): 
+    return a + b 
 
-def add(a, b):
-    return a + b
-    
-ask = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:\n"))
+def sub(a, b): 
+    return a - b 
 
-a1 = float(input("podaj pierszy składnik:\n"))
-a2 = float(input("podaj drugi składnik:\n"))
+def mul(a, b): 
+    return a * b 
 
-if ask == 1:
-    logging.info(f"Dodaję {a1} oraz {a2}.")
-    print(add(a1,a2))
-elif ask == 2:
-    pass
-elif ask == 3:
-    pass
-elif ask == 4:
-    pass
+def div(a, b): 
+    return a / b 
+
+ask = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:\n")) 
+
+num1 = int(input("podaj pierszy składnik:\n")) 
+num2 = int(input("podaj drugi składnik:\n")) 
+
+if ask == 1: 
+    logging.info(f"Dodaję {num1} oraz {num2}.") 
+    print(add(num1,num2)) 
+elif ask == 2: 
+    logging.info(f"Od {num1} odejmuję {num2}.") 
+    print(sub(num1,num2)) 
+elif ask == 3: 
+    logging.info(f"Mnożę {num1} razy {num2}.") 
+    print(mul(num1,num2)) 
+elif ask == 4: 
+    logging.info(f"Dzielę {num1} przez {num2}.") 
+    print(div(num1,num2))
